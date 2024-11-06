001cae20b99257f450b1a894bcd59f7bee5a1990
0xsterat <atharva.tayade555@gmail.com>
2024-11-06 17:16:44 +0530

    actions

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+//jkbjhg
 console.log('main.tsx is running')
 
 const root = document.getElementById('root')
